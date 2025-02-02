e439b8c4174464a035c2e36118a4c6b0478c3575
personalleandrocesar <leandrocesar.goo@gmail.com>
2025-02-02 22:47:32 +0000

    updt

--- hydrium.sh
+++ hydrium.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+git pull
+
 # Executa o npm run generate
 npm run generate
 
 # Remove todos os arquivos em /public_html
-rm -rf /home/hydrium.co/public_html/*
+rm -rf /home/deepacupuntura.com/html/*
 
-# Copie os novos arquivos gerados para /public_html
-cp -r /home/hydrium.co/hydrium/dist/* /home/hydrium.co/public_html
+# Copia os novos arquivos gerados para /public_html
+cp -r /home/leandro/hydriun.com/dist/* /var/www/hydriun.com/html
